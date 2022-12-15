ec419298d3aea8d64285d4445cf9dfd4c320937b
Ousman <usman.traid@gmail.com>
2022-12-15 08:41:57 -0500

    code refactored

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,47 @@
 #include "binary_trees.h"
-size_t subtree_height(const binary_tree_t *type);
-int subtrees_heights_are_equal(const binary_tree_t *type);
+
+size_t binary_tree_leaves(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
+/**
+ * binary_tree_leaves - count the leaves in a binary tree
+ * @tree: the pointer to the root node of the binary tree
+ *
+ * Return: the count, 0 if tree is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t count;
+
+		count = (!tree->left && !tree->right) ? 1 : 0;
+		count += binary_tree_leaves(tree->left);
+		count += binary_tree_leaves(tree->right);
+		return (count);
+	}
+
+	return (0);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: the root of the tree whose height is to be measured
+ *
+ * Return: height of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h_l = 0, h_r = 0;
+
+	if (tree)
+	{
+		h_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		h_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return (h_l > h_r ? h_l : h_r);
+	}
+	return (0);
+}
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
@@ -10,47 +51,22 @@ int subtrees_heights_are_equal(const binary_tree_t *type);
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		if (tree->left && tree->right &&
-		    subtrees_heights_are_equal(tree))
-			return (1);
+	size_t height, leaf_n_height_rel = 1, leaves;
+
+	if (!tree)
 		return (0);
-	}
 
-	return (0);
-}
+	height = binary_tree_height(tree);
+	leaves = binary_tree_leaves(tree);
 
-/**
- * subtree_height - measures the height of a binary subtree
- * @tree: pointer to the root node of the binary tree
- *
- * Return: measured height, or -1 if tree is NULL
- */
-size_t subtree_height(const binary_tree_t *tree)
-{
-	size_t l, r;
-
-	if (tree)
+	while (height)
 	{
-		l = tree->left ? 1 + subtree_height(tree->left) : 0;
-		r = tree->right ? 1 + subtree_height(tree->right) : 0;
-		return (l > r ? l : r);
+		leaf_n_height_rel *= 2;
+		height--;
+
 	}
 
-	return (-1);
-}
-
-/**
- * subtrees_heights_are_equal - checks if the heights of subtrees of a
- *                              binary tree node are equal
- * @tree: the pointer to the node whose subtrees heights are to be compared
- *
- * Return: 1 if they are equal, 0 otherwise
- */
-int subtrees_heights_are_equal(const binary_tree_t *tree)
-{
-	if (subtree_height(tree->left) == subtree_height(tree->right))
+	if (leaves == leaf_n_height_rel)
 		return (1);
 	return (0);
 }
